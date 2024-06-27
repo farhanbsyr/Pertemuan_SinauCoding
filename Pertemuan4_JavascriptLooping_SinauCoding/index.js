e31c6085f38a2f0c1eb06c19a6e7d3653fba743b
unknown <farhanbsyr@gmail.com>
2024-06-27 11:27:20 +0700

    first commit

---
+++ Pertemuan4_JavascriptLooping_SinauCoding/index.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++) {
+  if (i < 7) {
+    console.log("Bus Transjakarta " + i + " beroperasi dengan baik.");
+  } else if (i === 8) {
+    console.log("Bus Transjakarta " + i + " sedang lembur.");
+  } else {
+    console.log("Bus Transjakarta " + i + " tidak beroperasi dengan baik.");
+  }
+}
